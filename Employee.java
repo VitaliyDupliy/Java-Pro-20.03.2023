224e77c8758b38d3720dc1931f650f36d1ef0061
Dupliy Vitaliy <gidrohlorid9@gmail.com>
2023-03-30 15:02:43 +0300

    Generate and change toString

---
+++ Employee.java
@@ -0,0 +1,30 @@
+package homework3;
+
+public abstract class Employee  {
+	
+	
+	String firstName, lastName;
+
+	public Employee(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	abstract double salaryCalc();
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + "[firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
+	
+		
+
+
+	
+	
+	
+	
+
+}
