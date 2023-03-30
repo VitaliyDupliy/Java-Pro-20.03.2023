e21d0fa938f8f8d174b3c196ebd9a3f10ab03a40
Dupliy Vitaliy <gidrohlorid9@gmail.com>
2023-03-30 15:33:06 +0300

    add PieceWorker.java

---
+++ PieceWorker.java
@@ -0,0 +1,30 @@
+package homework3;
+
+public class PieceWorker extends Employee {
+
+	
+	final int payForProd = 100;
+	int quantityOfProd;
+	int salaryPerMonth;
+	
+	public PieceWorker(String firstName, String lastName, int quantityOfProd) {
+		super(firstName, lastName);
+		this.quantityOfProd = quantityOfProd;
+		
+	}
+
+	@Override
+	double salaryCalc() {
+		salaryPerMonth = quantityOfProd * payForProd;	
+		return salaryPerMonth;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " payForProd=" + payForProd + ", quantityOfProd=" + quantityOfProd + ", salaryPerMonth="
+				+ salaryCalc() + "]";
+	}
+	
+	
+	
+}
