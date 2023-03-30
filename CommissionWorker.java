a38ff1f72a7bf5fcc5e077f3621abe4a9398cb1c
Dupliy Vitaliy <gidrohlorid9@gmail.com>
2023-03-30 15:23:53 +0300

    add CommissionWorker.java

---
+++ CommissionWorker.java
@@ -0,0 +1,36 @@
+package homework3;
+
+
+
+public class CommissionWorker extends Employee {
+	
+	final int basicSal = 3000;
+	double salaryPerMonth;
+	int sales;
+	final int persentOfSal = 30;
+
+	public CommissionWorker(String firstName, String lastName, int sales) {
+		super(firstName, lastName);
+		this.sales = sales;
+	
+	}
+
+	@Override
+	double salaryCalc() {
+		salaryPerMonth = (double)(basicSal  + sales * persentOfSal / 100);
+		
+		
+		
+		return salaryPerMonth ;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " basicSal=" + basicSal + ", sales=" + sales + ", salaryPerMonth=" + salaryCalc() + "]";
+	}
+
+	
+
+	
+
+}
