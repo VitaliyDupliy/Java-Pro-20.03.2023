6ea58540b078f3cef63c509e3113c3043b9094a7
Dupliy Vitaliy <gidrohlorid9@gmail.com>
2023-03-30 15:17:54 +0300

    change toString in   hourlyWorker

--- HourlyWorker.java
+++ HourlyWorker.java
@@ -30,7 +30,7 @@ public class HourlyWorker extends Employee {
 	@Override
 	public String toString() {
 		return super.toString() + " normOfHours= " + normOfHours + ", hoursWorked= " + hoursWorked + ", hourlyRate= "
-				+ hourlyRate + ", salaryPerMonth= " + salaryCalc() + "]";
+				+ hourlyRate + ", salaryPerMonth= " + "]";
 	}
 	
 
